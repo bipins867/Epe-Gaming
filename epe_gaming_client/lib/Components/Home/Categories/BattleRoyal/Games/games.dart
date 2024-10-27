dfcd806ab3d4f65cd0591d68261c2dfae26e06b6
bipins867 <bipins867@gmail.com>
2024-10-27 10:11:52 +0530

    Added the basic games ...

---
+++ epe_gaming_client/lib/Components/Home/Categories/BattleRoyal/Games/games.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Games extends StatelessWidget {
+  const Games({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        _buildGameCard('BGMI', 'assets/BattleRoyal/BGMI/bgmi.jpg'),
+        _buildGameCard('Free Fire', 'assets/BattleRoyal/FreeFire/freefire.jpg'),
+        _buildGameCard('Call of Duty', 'assets/BattleRoyal/COD/cod.jpg'),
+      ],
+    );
+  }
+
+  Widget _buildGameCard(String title, String imagePath,
+      {VoidCallback? callbackFunc}) {
+    return GestureDetector(
+      onTap: callbackFunc,
+      child: Card(
+        elevation: 4,
+        margin: const EdgeInsets.all(8), // Added margin for game cards
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- epe_gaming_client/lib/Components/Home/Categories/categories.dart
+++ epe_gaming_client/lib/Components/Home/Categories/categories.dart
@@ -1,3 +1,4 @@
+import 'package:epe_gaming_client/Components/Home/Categories/BattleRoyal/battleRoyal.dart';
 import 'package:flutter/material.dart';
 
 class Categories extends StatelessWidget {
@@ -21,7 +22,15 @@ class Categories extends StatelessWidget {
             physics: NeverScrollableScrollPhysics(),
             children: [
               _buildCategoryCard(
-                  'BattleRoyal', 'assets/BattleRoyal/battleRoyal.jpg'),
+                'BattleRoyal',
+                'assets/BattleRoyal/battleRoyal.jpg',
+                callbackFunc: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const BattleRoyal()),
+                  );
+                },
+              ),
               _buildCategoryCard('PC Games', 'assets/feature.jpg'),
               _buildCategoryCard('Board Games', 'assets/feature.jpg'),
             ],
@@ -31,30 +40,34 @@ class Categories extends StatelessWidget {
     );
   }
 
-  Widget _buildCategoryCard(String title, String imagePath) {
-    return Card(
-      elevation: 4,
-      margin: EdgeInsets.all(8), // Added margin for category cards
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(imagePath,
-                  fit: BoxFit.cover), // Fixed height image
+  Widget _buildCategoryCard(String title, String imagePath,
+      {VoidCallback? callbackFunc}) {
+    return GestureDetector(
+      onTap: callbackFunc,
+      child: Card(
+        elevation: 4,
+        margin: const EdgeInsets.all(8), // Added margin for category cards
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(imagePath,
+                    fit: BoxFit.cover), // Fixed height image
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              title,
-              style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold), // Decreased font size
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold), // Decreased font size
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
