060636b784e312ad3edf142012c121e92db2f497
bipins867 <bipins867@gmail.com>
2024-11-05 20:41:00 +0530

    Now creting anythor branch for ntoficationHandler....

--- epe_gaming_client/lib/Store/baseStoreProvider.dart
+++ epe_gaming_client/lib/Store/baseStoreProvider.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class BaseStoreProvider extends ChangeNotifier {
   Map<String, dynamic>? userProfileInfo;
+  bool isProfileLoading = false;
 
   updateUserProfileInfo(Map<String, dynamic> userProfileInfo) {
     this.userProfileInfo = userProfileInfo;
+    notifyListeners();
+  }
+
+  updateIsProfileLoadingStatus(bool cond) {
+    isProfileLoading = cond;
     notifyListeners();
   }
 }
