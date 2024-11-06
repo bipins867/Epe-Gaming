13cd8465171dfbd79ff71e40365fd800212d44c0
bipins867 <bipins867@gmail.com>
2024-11-07 00:16:32 +0530

    Minor changes in app...

--- epe_gaming_client/lib/Components/Profile/NavigationItems/Legality/ReturnRefundAndCancellationPolicy/returnRefundAndCancellationPolicy.dart
+++ epe_gaming_client/lib/Components/Profile/NavigationItems/Legality/ReturnRefundAndCancellationPolicy/returnRefundAndCancellationPolicy.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ReturnRefundCancellationPolicy extends StatelessWidget {
+  const ReturnRefundCancellationPolicy({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
